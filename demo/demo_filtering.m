8699148e650173741fe579731eb2d68094476a8c
fenmeng123 <350319027@qq.com>
2023-11-20 16:15:07 +0800

    Add HRV Calculating

--- demo/demo_filtering.m
+++ demo/demo_filtering.m
@@ -1,4 +1,5 @@
-load PhysIO_Outputs\physio.mat
+%% Apply Butterworth filter on ECG signal data
+load 20231114_lixin\physio.mat
 
 % Your ECG signal variable and sampling frequency
 ecg_signal = physio.ons_secs.c;% (Your ECG signal data here)
@@ -22,3 +23,32 @@ plot(t, filtered_ecg);
 title('Filtered ECG Signal');
 xlabel('Time (s)');
 ylabel('Amplitude');
+
+
+%% Extract R-peaks from filtered ECG signal and calculate HRV
+cpulse_detect_options.method = 'auto_matched';
+cpulse_detect_options.min = 0.4;
+cpulse_detect_options.file = 'initial_cpulse_kRpeakfile.mat';
+cpulse_detect_options.max_heart_rate_bpm = 120;
+cpulse_detect_options.kRpeak = [];
+
+physio_verbose.close_figs = false;
+physio_verbose.level = 0;
+physio_verbose.fig_output_file = '';
+physio_verbose.use_tabs = true;
+physio_verbose.fig_handles = [];
+
+
+[cpulse, ~] = tapas_physio_get_cardiac_pulses(t, filtered_ecg, ...
+    cpulse_detect_options, 'ECG', physio_verbose);
+%   cpulse - vector of onset-times (in seconds) of occuring heart beats
+
+HRV = s_ECG_HRVcalc(cpulse,[]);
+
+% 在计算心率变异性(HRV)的频域特征时，LF/HF比率中的频率带宽通常定义如下：
+% 
+% 低频（LF）范围：0.04至0.15 Hz，反映交感和副交感神经系统的活动。
+% 高频（HF）范围：0.15至0.4 Hz，主要反映副交感神经系统的活动。
+% LF/HF比率被用来评估交感神经和副交感神经之间的平衡。高的LF/HF比率可能指示交感神经系统活动增加，而低的比率可能指示副交感神经系统的主导。
+disp(HRV.time)
+disp(HRV.freq)
--- s_ECG_ExtractRwave.m
+++ s_ECG_ExtractRwave.m
@@ -1,7 +1,13 @@
 function [R_locs, HRV] = s_ECG_ExtractRwave(ecg_signal, Fs)
 % Get the location of R-wave peaks in ECG signal
 % 
-% 
+% Input:
+%   Positional Argumentions:
+%       ecg_signal (Required) - a vector contains the ECG signal data
+%       Fs (Optional) - a scalar indicates the frequency of sampling, in Hz. Default
+%       is 400 Hz.
+% Output:
+%   R_locs - the detected R-wave peaks, which are aligned 
 % Written By Kunru Song 2023.11.20
 
 if nargin == 1
@@ -15,7 +21,7 @@ wtrec(4:5,:) = wt(4:5,:);
 y = imodwt(wtrec, 'sym4');
 
 % Find R-wave Peaks
-[~, R_locs] = findpeaks(y, 'MinPeakHeight', 0.5, 'MinPeakDistance', Fs/2.5);
+[~, R_locs] = findpeaks(y, 'MinPeakHeight', 0.4, 'MinPeakDistance', Fs/2.5);
 
 % Calculate HRV
 RR_intervals = diff(R_locs) / Fs; % RR间期，单位为秒
