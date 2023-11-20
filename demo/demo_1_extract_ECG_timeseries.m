a451271907f5af91640c82f8e1a3ef16876b46bb
fenmeng123 <350319027@qq.com>
2023-11-20 21:03:00 +0800

    update demos

--- demo/demo_1_extract_ECG_timeseries.m
+++ demo/demo_1_extract_ECG_timeseries.m
@@ -1,17 +1,20 @@
+% Demo script for extracting ECG time series from a Siemens PMU ECG file
+% 
+% Kunru Song 2023.11.16
+%% Set input arguments
 %   c                   cardiac time series (ECG or pulse oximetry)
 %   r                   respiratory time series
 %   t                   vector of time points (in seconds)
 %   cpulse              time events of R-wave peak in cardiac time series (seconds)
 
-
 LogFile_PARAMS = struct('vendor','Siemens', ...
-    'cardiac','E:\研究生\科研工作\231104磁共振生理数据处理\fMRI_PhysioPreps\demo\20231114_lixin.ecg', ...
+    'cardiac','./20231114_lixin.ecg', ...
     'respiration',{''},...
     'sampling_interval',0.0025, ...
     'relative_start_acquisition',0);
-LogFile_PARAMS.scan_timing = {'E:\研究生\科研工作\231104磁共振生理数据处理\fMRI_PhysioPreps\demo\20231114_Y02_ZH.MR.PROJECT_BNU-ZJT.2.1.2023.11.14.11.13.39.866.30729809.dcm';...
-    'E:\研究生\科研工作\231104磁共振生理数据处理\fMRI_PhysioPreps\demo\20231114_Y02_ZH.MR.PROJECT_BNU-ZJT.2.240.2023.11.14.11.13.39.866.30732438.dcm'};
+LogFile_PARAMS.scan_timing = {'./20231114_Y02_ZH.MR.PROJECT_BNU-ZJT.2.1.2023.11.14.11.13.39.866.30729809.dcm'};
 LogFile_PARAMS.align_scan = 'first';
+
 sqpar.Nslices = 30;
 sqpar.NslicesPerBeat = [];
 sqpar.TR = 2;
@@ -20,11 +23,12 @@ sqpar.Nscans = 240;
 sqpar.onset_slice = 1;
 sqpar.time_slice_to_slice = [];
 sqpar.Nprep = [];
+
 verbose.level = 0;
 verbose.fig_handles = [];
-
+%% extract time series from a ECG file
 [c, r, t, cpulse, verbose] = ...
     s_read_physlogfiles_siemens(LogFile_PARAMS,'ECG', verbose, ...
     'sqpar', sqpar);
-
-% plot(physio.ons_secs.c(1:10))
+%% Visualization of ECG time series
+plot(c(1:10))
